1ef7e91632be884da8efcd3ac8bd5fab8ba93ae7
Daniel Nasiloski <danielnasiloski@MacBook-Pro-de-Daniel.local>
2020-05-25 21:48:11 -0300

    Corrigido CPF estático na tela de cadastro

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -9,7 +9,6 @@ export default function Student({ history }){
     const [student, setStudent] = useState([]);
     const [name, setName] = useState();
     const [email, setEmail] = useState();
-    const [cpf, setCpf] = useState();
 
     const { ra_id } = useParams();
     const token = localStorage.getItem('token');
@@ -36,19 +35,13 @@ export default function Student({ history }){
         event.preventDefault();
         let std_name = name;
         let std_email = email;
-        let std_cpf = cpf;
         if(!std_name) std_name = student.nome;
         if(!std_email) std_email = student.email;
-        if(!std_cpf) std_cpf = student.cpf;
-
-        if(std_cpf.length < 11){
-            return notify.warn('O campo CPF deve possuir pelo menos 11 caracteres.')
-        }
 
         const data = {
             name: std_name,
             email: std_email,
-            cpf: std_cpf,
+            cpf: student.cpf,
             ra: ra_id
         }
         
@@ -95,11 +88,10 @@ export default function Student({ history }){
                 <label>CPF:</label>
                 <input 
                     placeholder={student.cpf} 
-                    pattern="([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"
                     type="cpf"
                     id="cpf"
-                    defaultValue={cpf}
-                    onChange={event => setCpf(event.target.value)}
+                    defaultValue={student.cpf}
+                    disabled
                 />
 
                 <div className="botoes">
